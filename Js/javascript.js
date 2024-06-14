641cfcd286fad8a9cb715408b801c890e3c52abf
Akif <ali-akif@live.fr>
2024-06-14 21:23:46 +0200

    wtf is a kilometer

---
+++ Js/javascript.js
@@ -0,0 +1,22 @@
+console.log("That's the print.")
+
+let varname;
+varname = 'Hello'
+console.log(varname)
+// alert(varname)
+let user = "Ali", age =" 21", message =" Salam";
+const myBirthday = "03.10.2002";
+
+// Uppercase is generally used for HArd coded variables, something we give to the computer.
+
+let a = 50, b = 50;
+let x = a + b;
+console.log(x)
+
+a++
+console.log(a) // 51
+
+x--
+console.log(x) // 99
+
+// ** exp or Math.pow(x,y) / divice, % remainder...
