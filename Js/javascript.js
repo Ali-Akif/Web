898a66624d37980e06f90315b6d3130c02091d19
Akif <ali-akif@live.fr>
2024-06-20 07:30:24 +0200

    string methods

--- Js/javascript.js
+++ Js/javascript.js
@@ -16,3 +16,53 @@ Number(stringNumberVariable)
 == and === // == test the value, === do it with the datatype too
 
 +var // will get "number" to the number
+
+const name = "Ali";
+const greeting = `Hello, ${name}` // `` is a template literal
+// you can use ${} only with template literal
+
+const newLine = `One day you finally knew
+what you had to do, and began `;
+
+const newLine = "One day you finally knew\nwhat you had to do, and began"
+
+Number() String()
+
+let text="bonjour";
+
+text.length;
+
+text.charAt()
+text.charCodeAt() // UTF-16 code
+text.at() // allow negative position
+text[] // read only
+
+text.slice(start, end)
+text.substring(s, e) // s and e value less than 0 are treated as 0
+text.substr(s, length)
+
+text.toUpperCase()
+text.toLowerCase()
+
+text.concat(" ", text2); // text + " " + text2
+
+text.trim() // remove whitespace from both side
+text.trimStart() 
+text.trimEnd()
+
+text.padStart(4, "x") // will add 4 x at start
+text.padEnd() // both are not supported on explorer
+
+text.repeat(2) // not supported on explorer
+
+text.replace("bonjour", "hi")
+text.replace(/BONJOUR/i, "hi") // case insensitive
+
+text.replaceAll("bon", "Jour") // ! explorer
+
+text.split(",") // return array
+text.split("") // array of single characters
+
+
+
+
