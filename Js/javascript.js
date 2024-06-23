ecd741c0304711d057f94d6c33e396d9eed3347a
Akif <ali-akif@live.fr>
2024-06-23 13:09:13 +0200

    Finally finished 2nd chapter !

--- Js/javascript.js
+++ Js/javascript.js
@@ -55,7 +55,7 @@ text.padEnd() // both are not supported on explorer
 
 text.repeat(2) // not supported on explorer
 
-text.replace("bonjour", "hi")
+text.replace("bonjour", "hi")     
 text.replace(/BONJOUR/i, "hi") // case insensitive
 
 text.replaceAll("bon", "Jour") // ! explorer
@@ -101,13 +101,70 @@ Boolean()
 ?? //Nullish coalescing
 
 
-switch (expression) {
+const greating = isBirthday
+    ? "Happy birthday !"
+    : "Good morning."
 
-    case choice 1;
-        // code
-        break;
+let result = condition ? value1 : value2;
 
-    case choice 2;
-        // code
-        break;
+// condition is evaluated, if it is truthy then value1 is returned else value2
+
+let accessAllowed = ( age > 18 ) ? true : false;
+
+
+
+let age = prompt('age?', 18); // will prewrite 18
+let message = (age < 3) ? 'Hi baby!' :
+    (age < 18) ? 'Hello!' :
+    (age < 100) ? 'Greetings!' :
+    'What an unusual age!';
+alert( message );
+
+if (age < 3) {
+    message = 'Hi, baby!';
+  } else if (age < 18) {
+    message = 'Hello!';
+  } else if (age < 100) {
+    message = 'Greetings!';
+  } else {
+    message = 'What an unusual age!';
+  }
+
+
+let userResponse = prompt('who created js');
+if (userResponse === 'he') {
+    alert("ye");
+} else {
+    alert("jo");
 }
+
+let value = prompt('Type a number', 0);
+
+if (value > 0) {
+    alert( 1 );
+} else if (value < 0) {
+    alert( -1 );
+} else {
+    alert( 0 );
+}
+
+let result2 = (a+b < 4) ? 'Below' : 'Over';
+
+
+let a = 2 + 2;
+switch (a) {
+    case 3: // if a === 3
+        alert('Too small');
+        break;
+    case 4:
+        alert('Exactly');
+        break;
+    case 5:
+    case 6:
+        alert('Too big');
+        break;
+    default:
+        alert("I don't know such values");
+}
+
++prompt()    prompt() // with the + the value will convert the string to number
