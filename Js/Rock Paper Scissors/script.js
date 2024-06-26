30949bcdd37b9acbfbcc288f3560496e48e9042c
Akif <ali-akif@live.fr>
2024-06-26 20:03:26 +0200

    This is sketchy as fuck

--- Js/Rock Paper Scissors/script.js
+++ Js/Rock Paper Scissors/script.js
@@ -1,3 +1,4 @@
+// Functions
 function getRandomInt() {
 
     let int = Math.floor(Math.random() * 3);
@@ -29,4 +30,58 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+function getUserChoice() {
+    let userInput = prompt(`0 for Rock 
+1 for Paper 
+2 for Scissors`);
+
+    if (userInput === null) {
+        return "Give me something to choose man"
+    }
+
+    let userChoice = NumberToChoice(Number(userInput));
+    return userChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let roundWinner = findWinner(humanChoice, computerChoice);
+    if (roundWinner === 1) {
+        humanScore +=1
+        return "You won!"
+    } else {
+        computerScore += 1;
+        return "You lost!"
+    };
+
+}
+
+function findWinner(choice1, choice2) {
+    const winningCombinations = {
+        Rock: 'Scissors',
+        Paper: 'Rock',
+        Scissors: 'Paper'
+    };
+
+    let winner = undefined;
+
+    if (choice1 === choice2) {
+        return "It's a tie!";
+    }
+
+    if (winningCombinations[choice1] === choice2) {
+        winner = 1
+    } else {
+        winner = 0
+    }
+    return winner
+}
+
+
+function playGame() {
+}
+
+// Code
+let humanScore = 0
+let computerScore = 0
+
+console.log(playRound(getUserChoice(), getComputerChoice()))
