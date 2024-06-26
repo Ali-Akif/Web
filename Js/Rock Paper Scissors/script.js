2b40f50258112ad18c5041235c876688a41b1548
Akif <ali-akif@live.fr>
2024-06-26 19:28:12 +0200

    Get Computer Choice

--- Js/Rock Paper Scissors/script.js
+++ Js/Rock Paper Scissors/script.js
@@ -1,1 +1,32 @@
-console.log('helzzzlo')
+function getRandomInt() {
+
+    let int = Math.floor(Math.random() * 3);
+    return int
+  }
+  
+ function NumberToChoice(number) {
+    let choice;
+    switch (number) {
+        case 0:
+            choice = "Rock";
+            break;
+        case 1:
+            choice = "Paper";
+            break;
+        case 2:
+            choice = "Scissors";
+            break;
+        default:
+            choice = "invalid choice";
+            break;
+    }
+    return choice;
+ } 
+ 
+function getComputerChoice() {
+    let number = getRandomInt();
+    let computerChoice = NumberToChoice(number);
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
