91c6383888ab91357ec71a424772d25c3e11e75d
Akif <ali-akif@live.fr>
2024-07-02 07:12:24 +0200

    Testing arrays

--- Js/test.js
+++ Js/test.js
@@ -183,12 +183,17 @@
 //   ]
 // }
 
-const myArray = [1,2,3,4,5,6,7,8,9,10];
-let sum = 0;
+// const myArray = [1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
 
 
-for (let i = 0; i < myArray.length; i++) {
-  sum += myArray[i]
-}
+// for (let i = 0; i < myArray.length; i++) {
+//   sum += myArray[i]
+// }
 
-console.log(sum)
+// console.log(sum)
+
+const a = ["A","B"];
+a.push("C")
+a[2] = [ 1, 2, 3 ]
+console.log(a)
