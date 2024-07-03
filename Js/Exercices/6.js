11316cd9d6a27a51cc8850df7a22d6347a6b9df4
Akif <ali-akif@live.fr>
2024-07-03 21:44:18 +0200

    Update

---
+++ Js/Exercices/6.js
@@ -0,0 +1,15 @@
+function  leapYears(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+  
+    if (
+      isYearDivisibleByFour &&
+      (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
