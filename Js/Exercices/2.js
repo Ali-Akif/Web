10403fdf11bf6f0db6dcfbeeed0f48135e06f08b
Akif <ali-akif@live.fr>
2024-07-03 20:54:31 +0200

    2ndé

---
+++ Js/Exercices/2.js
@@ -0,0 +1,11 @@
+function repeatString(string, number) {
+    let repeatedString = ""
+    for ( let i = 0; i<number; i++) {
+        repeatedString += string;
+    };
+    return repeatedString
+}
+
+
+string = repeatString("Hello", 3);
+console.log(string)
