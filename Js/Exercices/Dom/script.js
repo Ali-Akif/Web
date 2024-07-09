18193b7a0594e517cd5c2749fe7ed6499eda36e5
Akif <ali-akif@live.fr>
2024-07-09 23:31:19 +0200

    Final

--- Js/Exercices/Dom/script.js
+++ Js/Exercices/Dom/script.js
@@ -1,8 +1,27 @@
 // your JavaScript file
+
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+const redParagraph = document.createElement("p");
+redParagraph.textContent = "Hey I'm red!";
+redParagraph.style.color = "red";
+container.appendChild(redParagraph);
 
-container.appendChild(content);
+const blueh3 = document.createElement("h3");
+blueh3.textContent = "Hi I'm blue h3!";
+blueh3.style.color = "blue";
+container.appendChild(blueh3);
+
+const div = document.createElement("div");
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink";
+
+const h1InDiv = document.createElement("h1");
+h1InDiv.textContent = "I'm a div";
+div.appendChild(h1InDiv);
+
+const pInDiv = document.createElement("p");
+pInDiv.textContent = "Me too !";
+div.appendChild(pInDiv);
+
+container.appendChild(div);
