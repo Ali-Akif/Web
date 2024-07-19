7346efdf59ad9ac4896e4df4ea9a0fcfadc767ee
Akif <ali-akif@live.fr>
2024-07-19 04:22:44 +0200

    Update

--- Exercism_Recap/1st Part/1st layer/Numbers.js
+++ Exercism_Recap/1st Part/1st layer/Numbers.js
@@ -1,5 +1,5 @@
 // Numbers 
-// number, bigint, math.floor(), math.ceil()
+// number, bigint, math.floor(), math.ceil(), operators, +=
 
 
 // number, contain negatives, int, float, theses types doesn't exist on js
@@ -8,3 +8,13 @@
 Math.floor(234.34); // => 234
 Math.ceil(234.34); // => 235
 
+// Operators are + - * / % **
+
+2 - 1.5; //=> 0.5
+19 / 2; //=> 9.5
+-11 % 4; // => -3
+4 ** 1 / 2; // => 2
+
+let x = 5;
+x += 25; // x is now 30
+// += is applicable to all operators
