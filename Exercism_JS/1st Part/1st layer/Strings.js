c819ae8dea51e52ffe1d2eb43c80130d6beeacc4
Akif <ali-akif@live.fr>
2024-07-19 04:30:22 +0200

    Update

---
+++ Exercism_JS/1st Part/1st layer/Strings.js
@@ -0,0 +1,16 @@
+// String
+
+
+'cat'[1]; // => 'a'
+'cat'.length; // => 3
+
+'I like' + ' ' + 'cats.'; // => "I like cats."
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#instance_methods
+
+toUpperCase and toLowerCase - change the case of all characters
+trim - remove whitespace at the beginning and end
+includes, startsWith and endsWith - determine whether another string is part of the given string
+slice - extract a section of the string
+*/
