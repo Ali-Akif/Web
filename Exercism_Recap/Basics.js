321fb6ddc1f192de154ae9cced51ca6f3c86ba3b
Akif <ali-akif@live.fr>
2024-07-19 03:45:49 +0200

    Final Basic v

--- Exercism_Recap/Basics.js
+++ Exercism_Recap/Basics.js
@@ -7,21 +7,18 @@
 
 let myFirstVariable = 1;
 myFirstVariable = 'Some string';
-myFirstVariable = new SomeComplexClass();
-
 const MY_FIRST_CONSTANT = 10;
 // Can not be re-assigned.
-MY_FIRST_CONSTANT = 20;
-// => TypeError: Assignment to constant variable.
-
 
 function add(num1, num2) {
     return num1 + num2;
   }
 add(1, 3); // => 4
 
+
 // file.js
 export const MY_VALUE = 10;
+export {alreadyDefinedVariable};
 export function add(num1, num2) {
   return num1 + num2;
 }
