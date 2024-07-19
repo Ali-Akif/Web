d00d44c15fb4db5941dfebc36061b78728266437
Akif <ali-akif@live.fr>
2024-07-19 03:04:02 +0200

    f*ck

---
+++ Js/practice.js
@@ -0,0 +1,26 @@
+// Exercice 1 
+
+const name = "Akif";
+let age = 21;
+let city = "Strasbourg";
+alert(`Hello, I am ${name}, ${age} years old, from ${city}, nice to meet you !`);
+
+// Exercice 2
+
+function isPair(number) {
+    const result = Number(number) % 2 === 0 ? "Even":"Odd";
+    return result
+}
+
+// Exercice 3
+
+for ( i = 0; i< 10; i++) {
+    console.log(i);
+}
+
+// Exercice 4
+
+function reverseString(string) {
+    let newString = "";
+    for ()
+}
