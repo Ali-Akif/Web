51447d26b88110cfb8f9c1413bda020fdebbe82c
Akif <ali-akif@live.fr>
2024-07-20 07:12:29 +0200

    Update

---
+++ Exercism_JS/2nd Part/1st layer/Functions.js
@@ -0,0 +1,33 @@
+// Functions
+
+function someName(param1, param2, param3) {
+    // ...
+  }
+
+//For all other values (objects, arrays, functions), a reassignment will not affect the original value. However, if you modify such an argument (e.g. add a key to an object), that also modifies the original value that was passed in.
+
+function someName(param1 = defaultValue1, param2 = defaultValue2) { // Else automatic undefined
+    // ...
+  }
+
+function divide(a, b) {
+return {
+    quotient: Math.floor(a / b),
+    remainder: a % b,
+};
+}
+
+// Function expression
+const someFunction = function (param) {
+    // ...
+  };
+  
+  someOtherFunction(function (param) {
+    // ...
+  });
+  
+  const obj = {
+    someFunction: function (param) {
+      // ...
+    },
+  };
