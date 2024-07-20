d78750a2e69dfed13a17a19c37c7d5e22525cb20
Akif <ali-akif@live.fr>
2024-07-20 08:15:10 +0200

    Update

---
+++ Exercism_JS/2nd Part/1st layer/Callbacks.js
@@ -0,0 +1,18 @@
+// Callback functions
+
+const sideLength = 5;
+
+// Caller function takes a callback function
+function applySideLength(callback) {
+  return callback(sideLength);
+}
+
+// Callback must expect the possible argument from the calling function
+function areaOfSquare(side) {
+  return side * side;
+}
+
+applySideLength(areaOfSquare); // => 25
+
+
+applySideLength(function squarePerimeterLength(side) {return side * 4;});
