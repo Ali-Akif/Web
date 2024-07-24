a2c419a1e47aae0f8028ea5276d679cf6266c61b
Akif <ali-akif@live.fr>
2024-07-24 11:49:39 +0200

    Update

--- Exercism_JS/2nd Part/4th layer/ArrayLoops.js
+++ Exercism_JS/2nd Part/4th layer/ArrayLoops.js
@@ -1,1 +1,11 @@
-    
+    // Array Loop
+
+// for... of when you don't need the index
+
+let numbers = [6, 10, 23];
+for (const number of number) {
+    console.log(number); // 6 n/ 10 n/ 23
+}
+
+// forEach no break or continue
+numbers.forEach((number, index) => console.log(number, index)); // 6 0 n/ 10 1 n/ 23 2
