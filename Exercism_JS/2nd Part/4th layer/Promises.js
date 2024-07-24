37ff0ce4e7c978e8560d4248c4d746ded8ff1626
Akif <ali-akif@live.fr>
2024-07-24 12:58:15 +0200

    Update

--- Exercism_JS/2nd Part/4th layer/Promises.js
+++ Exercism_JS/2nd Part/4th layer/Promises.js
@@ -1,2 +1,22 @@
 // Promises
 
+const myPromise = new Promise(function (resolve, reject) {
+    let sampleData = [2, 4, 6, 8];
+    let randomNumber = Math.floor(Math.random() * (sampleData.length + 1));
+    if (sampleData[randomNumber]) {
+      resolve(sampleData[randomNumber]);
+    } else {
+      reject('An error occured!');
+    }
+  });
+  
+  myPromise
+    .then(function (e) {
+      console.log(e);
+    })
+    .catch(function (error) {
+      throw new Error(error);
+    })
+    .finally(function () {
+      console.log('Promise completed');
+    });
